70508951e17241f3aeecda96b0ef8cc2c6d3d747
Thomas Schweiger <tschweiger@edna.lan>
2019-02-10 07:29:03 -0800

    Sound now works, and a basic web server

--- app/src/main/java/net/starkadder/rfbplayer/MainActivity.java
+++ app/src/main/java/net/starkadder/rfbplayer/MainActivity.java
@@ -2,6 +2,7 @@ package net.starkadder.rfbplayer;
 
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
@@ -10,6 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,84 +23,90 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
     private boolean initialStage = true;
 
+    private RFBWebserver rfbweb;
+
+
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressDialog = new ProgressDialog(this);
         btn = (Button) findViewById(R.id.audioStreamBtn);
+        rfbweb =  new RFBWebserver();
+        new Server().execute();
+
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        progressDialog = new ProgressDialog(this);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-		public void onClick(View view) {
+            public void onClick(View view) {
                 if (!playPause) {
                     btn.setText("Pause Streaming");
 
                     if (initialStage) {
-                        new Player().execute("http://edna/goinghome.mp3");
+                        new Player().execute("http://edna/test.mp3");
                     } else {
                         if (!mediaPlayer.isPlaying())
                             mediaPlayer.start();
                     }
 
                     playPause = true;
-
                 } else {
                     btn.setText("Launch Streaming");
 
                     if (mediaPlayer.isPlaying()) {
                         mediaPlayer.pause();
                     }
-
                     playPause = false;
                 }
             }
-	    });
+        });
     }
 
     @Override
-	protected void onPause() {
+    protected void onPause() {
         super.onPause();
 
         if (mediaPlayer != null) {
             mediaPlayer.reset();
             mediaPlayer.release();
             mediaPlayer = null;
+            mediaPlayer = new MediaPlayer();
         }
     }
 
     class Player extends AsyncTask<String, Void, Boolean> {
         @Override
-	    protected Boolean doInBackground(String... strings) {
+        protected Boolean doInBackground(String... strings) {
             Boolean prepared = false;
 
             try {
                 mediaPlayer.setDataSource(strings[0]);
                 mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                     @Override
-			public void onCompletion(MediaPlayer mediaPlayer) {
+                    public void onCompletion(MediaPlayer mediaPlayer) {
                         initialStage = true;
                         playPause = false;
                         btn.setText("Launch Streaming");
                         mediaPlayer.stop();
                         mediaPlayer.reset();
                     }
-		    });
+                });
 
                 mediaPlayer.prepare();
                 prepared = true;
 
             } catch (Exception e) {
-                Log.e("MyAudioStreamingApp", e.getMessage());
+                Log.e("RFBPlayer", e.getMessage());
+             //   e.printStackTrace();
                 prepared = false;
             }
-
             return prepared;
         }
 
         @Override
-	    protected void onPostExecute(Boolean aBoolean) {
+        protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
 
             if (progressDialog.isShowing()) {
@@ -108,11 +118,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-	    protected void onPreExecute() {
+        protected void onPreExecute() {
             super.onPreExecute();
-
             progressDialog.setMessage("Buffering...");
             progressDialog.show();
         }
     }
+
+    class Server extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            Boolean prepared = false;
+            try {
+                rfbweb.start();
+            } catch (Exception e) {
+                Log.e("RFBPlayer", e.getMessage());
+                e.printStackTrace();
+                prepared = false;
+            }
+            return prepared;
+        }
+
+
+
+
+    }
+
 }
